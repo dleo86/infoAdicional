7abf86c55a5a791c4b6ce64d29633f2598149efb
leandro <dleo86@gmail.com>
2020-05-17 17:55:09 -0300

    Desarrollado en Javascript

---
+++ InfoAdicional.js
@@ -0,0 +1,15 @@
+function Mostrar1(obj, a){
+  var e=document.getElementById(a);
+  if(!e)
+    return true;
+  e.style.display="block"
+  return true;
+}
+ 
+function Mostrar2(obj, a){
+  var e=document.getElementById(a);
+  if(!e)
+    return true;
+  e.style.display="none"
+  return true;
+}
